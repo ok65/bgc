a0e393b573825b4683dafa21bf71a9a8c640fe41
Oliver <mail.ok65@googlemail.com>
2024-04-02 23:29:04 +0100

    dropping the json import, just grabbing key data out and throwing it into sql

--- bg_collection.py
+++ bg_collection.py
@@ -12,13 +12,12 @@ class BGCollection:
     def add(self, name: str, owner: str, dummy=True):
         bgg = BGGClient()
         game = bgg.game(name)
-        bgg_json = json.dumps(game.data())
-        query = ("INSERT INTO bg_coll VALUES "
-                 "(id, bgg_data, club_data, dummy)"
-                 "VALUES (%s, %s, %s, %s)")
+        query = ("INSERT INTO games VALUES "
+                 "(game_id, name, players_min, players_max, description)"
+                 "VALUES (%s, %s, %s, %s, %s)")
         with get_db() as db:
             cur = db.cursor()
-            cur.execute(query, (game.id, bgg_json, "{}", 1))
+            cur.execute(query, (game.id, game.name, game.min_players, game.max_players, game.description))
             cur.commit()
             cur.close()
             print(f"New game added, id: {game.id}")
