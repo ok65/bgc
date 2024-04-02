cbec2c3d5e19ab17a5bc1bb8f0ca411eb7f94098
Oliver <mail.ok65@googlemail.com>
2024-04-02 22:42:49 +0000

    tweaked bg_collection.py from within server bash, got it working

--- bg_collection.py
+++ bg_collection.py
@@ -12,13 +12,13 @@ class BGCollection:
     def add(self, name: str, owner: str, dummy=True):
         bgg = BGGClient()
         game = bgg.game(name)
-        query = ("INSERT INTO games VALUES "
-                 "(game_id, name, players_min, players_max, description)"
+        query = ("INSERT INTO games "
+                 "(game_id, name, players_min, players_max, description) "
                  "VALUES (%s, %s, %s, %s, %s)")
         with get_db() as db:
             cur = db.cursor()
             cur.execute(query, (game.id, game.name, game.min_players, game.max_players, "Description goes here"))
-            cur.commit()
+            db.commit()
             cur.close()
             print(f"New game added, id: {game.id}")
 
