f727b2aba8e77e6d2268c833097cb93fa6374cd5
Oliver <mail.ok65@googlemail.com>
2024-04-21 11:22:33 +0100

    testing crawler

--- crawler.py
+++ crawler.py
@@ -1,6 +1,7 @@
 
 from boardgamegeek import BGGClient
 import csv
+import json
 from database import get_db
 
 bgg = BGGClient()
@@ -13,25 +14,25 @@ with open(csv_file_path, "r") as fp:
 
     header = next(csv_reader)
 
-    json = {}
+
 
     for row in csv_reader:
         game_id = int(row[0])
 
         data = bgg.game(game_id=game_id).data()
 
-        name = data.get("name")
-        image_url = data.get("image")
-        thumb_url = data.get("thumbnail")
-        categories = ", ".join(data.get("categories", []))
-        designers = ", ".join(data.get("designers", []))
-        artists = ", ".join(data.get("artists", []))
-        players_min = int(data.get("min_players", 0))
-        players_max = int(data.get("max_players", 0))
-        playtime_min = int(data.get("min_playtime", 0))
-        playtime_max = int(data.get("max_playtime", 0))
-        mechanics = ", ".join(data.get("mechanics", []))
-        family = ", ".join(data.get("family", []))
+        name = json.dumps(data.get("name"))
+        image_url = json.dumps(data.get("image"))
+        thumb_url = json.dumps(data.get("thumbnail"))
+        categories = json.dumps(data.get("categories", []))
+        designers = json.dumps(data.get("designers", []))
+        artists = json.dumps(data.get("artists", []))
+        players_min = int(data.get("minplayers", 0))
+        players_max = int(data.get("maxplayers", 0))
+        playtime_min = int(data.get("minplaytime", 0))
+        playtime_max = int(data.get("maxplaytime", 0))
+        mechanics = json.dumps(data.get("mechanics", []))
+        family = json.dumps(data.get("families", []))
 
         query = ("INSERT INTO bgg_data "
                  "(game_id, name, image_url, thumb_url, categories, designers, artists, players_min, players_max, playtime_min, playtime_max, mechanics, family) "
@@ -39,14 +40,27 @@ with open(csv_file_path, "r") as fp:
 
         print(query)
 
-        with get_db() as db:
-            cur = db.cursor()
-            cur.execute(query)
-            db.commit()
-            cur.close()
+        try:
+            with get_db() as db:
+                cur = db.cursor()
+                cur.execute(query)
+                db.commit()
+                cur.close()
+
+        except:
+            print(" !!!! SQL insert failed")
+            print("  ")
+            continue
+
+        else:
+            print("SQL insert passed")
+            print("  ")
+
 
         pass
 
 
     print("##### ENTIRE DATASET COMPLETE #######")
     pass
+
+
