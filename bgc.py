ce630a9128ea3d7dc583161f79240f54227265da
ok65 <mail.ok65@googlemail.com>
2024-04-25 09:45:06 +0100

    better formatting

--- bgc.py
+++ bgc.py
@@ -9,10 +9,8 @@ class BGC:
     def game_search(cls, name: str, exact_match=False):
         with get_db() as db:
             cur = db.cursor()
-            if exact_match:
-                query = ("SELECT * FROM bgg_data WHERE (name = '%s');")
-            else:
-                query = ("SELECT * FROM bgg_data WHERE (name LIKE '%%s%');")
+            name = name if exact_match else f"%{name}%"
+            query = ("SELECT * FROM bgg_data WHERE (name LIKE %s);")
             cur.execute(query, [name])
             return cur.fetchall()
 
