611ef54ae70f214d3eb80772c2dc8085bcdfdac0
ok65 <mail.ok65@googlemail.com>
2024-04-25 10:39:48 +0100

    Updated crawler.py

--- crawler.py
+++ crawler.py
@@ -1,5 +1,5 @@
 
-from boardgamegeek import BGGClient
+from boardgamegeek import BGGClient, BGGApiRetryError
 import csv
 import json
 from database import get_db
@@ -19,86 +19,91 @@ session_count = 0
 
 if __name__ == "__main__":
 
-    with open(csv_file_path, "r") as fp:
-        csv_reader = csv.reader(fp)
+    while True:
+        with open(csv_file_path, "r") as fp:
+            csv_reader = csv.reader(fp)
 
-        header = next(csv_reader)
+            header = next(csv_reader)
+            for row in csv_reader:
+                game_id = int(row[0])
 
-        for row in csv_reader:
-            game_id = int(row[0])
-
-            with get_db() as db:
-                cur = db.cursor()
-                cur.execute(f"SELECT (game_id) FROM bgg_data WHERE (game_id={game_id})")
-                result = cur.fetchall()
-                db.commit()
-                cur.close()
-
-            if result:
-                print(f"skipping {game_id}, already in sql db")
-                continue
-
-            pass
-
-            time.sleep(1)
-
-            try:
-                data = bgg.game(game_id=game_id).data()
-            except Exception as error:
-                print(error)
-                continue
-             
-            if not isinstance(data, dict):
-                print(f"data bad, skipping game {game_id}")
-                continue 
-
-            name = escape(data.get("name", ""))
-            print(type(data.get("image", "")))
-            image_url = escape(data.get("image", ""))
-            thumb_url = escape(data.get("thumbnail", ""))
-            categories = ", ".join([escape(x) for x in data.get("categories", [])])
-            designers = ", ".join([escape(x) for x in data.get("designers", [])])
-            artists = ", ".join([escape(x) for x in data.get("artists", [])])
-            players_min = int(data.get("minplayers", 0))
-            players_max = int(data.get("maxplayers", 0))
-            playtime_min = int(data.get("minplaytime", 0))
-            playtime_max = int(data.get("maxplaytime", 0))
-            mechanics = ", ".join([escape(x) for x in data.get("mechanics", [])])
-            family = ", ".join([escape(x) for x in data.get("families", [])])
-
-            query = ("INSERT INTO bgg_data "
-                     "(game_id, name, image_url, thumb_url, categories, designers, artists, players_min, players_max, playtime_min, playtime_max, mechanics, family) "
-                     f"VALUES ({game_id}, '{name}', '{image_url}', '{thumb_url}', '[{categories}]', '[{designers}]', '[{artists}]', {players_min}, {players_max}, {playtime_min}, {playtime_max}, '[{mechanics}]', '[{family}]')")
-
-            print(query)
-
-            session_count += 1
-
-            print(session_count)
-
-
-            try:
                 with get_db() as db:
                     cur = db.cursor()
-                    cur.execute(query)
+                    cur.execute(f"SELECT (game_id) FROM bgg_data WHERE (game_id={game_id})")
+                    result = cur.fetchall()
                     db.commit()
                     cur.close()
 
-            except Exception as error:
-                print(" !!!! SQL insert failed")
-                print(error)
-                print("  ")
-                continue
+                if result:
+                    print(f"skipping {game_id}, already in sql db")
+                    break
 
-            else:
-                print("SQL insert passed")
-                print("  ")
+                data = None
+
+                for x in range(3):
+
+                    try:
+                        data = bgg.game(game_id=game_id).data()
+
+                    except Exception as error:
+                        time.sleep(1)
+
+                    else:
+                        break
+
+                if not isinstance(data):
+                    print(f"bad data, skipping game {game_id}")
+                    continue
+
+                name = escape(data.get("name", ""))
+                print(type(data.get("image", "")))
+                image_url = escape(data.get("image", ""))
+                thumb_url = escape(data.get("thumbnail", ""))
+                categories = ", ".join([escape(x) for x in data.get("categories", [])])
+                designers = ", ".join([escape(x) for x in data.get("designers", [])])
+                artists = ", ".join([escape(x) for x in data.get("artists", [])])
+                players_min = int(data.get("minplayers", 0))
+                players_max = int(data.get("maxplayers", 0))
+                playtime_min = int(data.get("minplaytime", 0))
+                playtime_max = int(data.get("maxplaytime", 0))
+                mechanics = ", ".join([escape(x) for x in data.get("mechanics", [])])
+                family = ", ".join([escape(x) for x in data.get("families", [])])
+                year_published = data.get("yearpublished ", 0)
+                description = escape(data.get("description", ""))
+
+                query = ("INSERT INTO bgg_data "
+                         "(game_id, name, image_url, thumb_url, categories, designers, artists, players_min, players_max, playtime_min, playtime_max, mechanics, family, year_published, description) "
+                         f"VALUES ({game_id}, '{name}', '{image_url}', '{thumb_url}', '[{categories}]', '[{designers}]', '[{artists}]', {players_min}, {players_max}, {playtime_min}, {playtime_max}, '[{mechanics}]', '[{family}]', {year_published}, '{description}')")
+
+                print(query)
+
+                session_count += 1
+
+                print(session_count)
 
 
+                try:
+                    with get_db() as db:
+                        cur = db.cursor()
+                        cur.execute(query)
+                        db.commit()
+                        cur.close()
+
+                except Exception as error:
+                    print(" !!!! SQL insert failed")
+                    print(error)
+                    print("  ")
+                    continue
+
+                else:
+                    print("SQL insert passed")
+                    print("  ")
+
+
+                pass
+
+
+            print("##### ENTIRE DATASET COMPLETE #######")
             pass
-
-
-        print("##### ENTIRE DATASET COMPLETE #######")
-        pass
 
 
